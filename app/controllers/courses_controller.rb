8dd60c14b766124a4b90275a334e4ec2a5a3655d
github username <github email address>
2020-04-07 22:47:50 -0400

    create index, new, and show actions

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,13 +1,24 @@
 class CoursesController < ApplicationController
   def index
+    @courses = Course.all
   end
 
   def new
+    @course = Course.new
   end
 
   def show
+    @course = Course.find(params[:id])
   end
 
   def edit
   end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name, :description, :location, :maximum_participants)
+  end
+
+
 end
