dbdbbbbdd7ac1e5f5fe652e9f2014a6eda9746ef
github username <github email address>
2020-04-09 18:01:33 -0400

    added create and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
+    #@wizard = Wizard.new #whyyy?
   end
+
+  def create
+    @wizard = Wizard.find_by(email: params[:email])
+    if @wizard && @wizard.authenticate(params[:password])
+      session[:wizard_id] = @wizard.id
+      redirect_to wizard_path(@wizard)
+    else
+      flash[:alert] = "Incorrect Sign In"
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete :wizard_id
+    redirect_to root_path
+  end
+
 end
