478f88b06bedc7d9af2ed8bdff865245fb34a444
github username <github email address>
2020-04-10 14:50:08 -0400

    add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    helper_method :current_wizard, :require_login, :admin_only
+
+    def current_wizard
+        if session[:wizard_id].present?
+            wizard = Wizard.find_by(:id => session[:wizard_id])
+        end
+    end
+
+    def require_login
+        unless current_wizard
+        redirect_to root_url
+        end
+    end
+
+    def admin_only
+        unless current_wizard.admin
+        flash[:notice] = "You must be an admin to perform that function!"
+        redirect_to wizard_path(current_wizard)
+        end
+    end
 end
