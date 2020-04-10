7db002c1e6cda0b9d0afeffea48aeffe1eb72ec5
github username <github email address>
2020-04-10 14:52:03 -0400

    add methods for edit and delete

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,6 @@
 class CoursesController < ApplicationController
+  before_action :find_course, only: [:show, :edit, :update, :destroy]
+  
   def index
     @courses = Course.all
   end
@@ -7,11 +9,26 @@ class CoursesController < ApplicationController
     @course = Course.new
   end
 
+  def create
+    @course = Course.new(course_params)
+		@course.save
+		redirect_to course_path(@course)
+  end
+
   def show
-    @course = Course.find(params[:id])
   end
 
   def edit
+  end
+
+  def update
+	  @course.update(course_params)
+	  redirect_to course_path(@course)
+  end
+
+  def destroy
+    @course.destroy
+    redirect_to courses_path
   end
 
   private
@@ -20,5 +37,8 @@ class CoursesController < ApplicationController
     params.require(:course).permit(:name, :description, :location, :maximum_participants)
   end
 
+  def find_course
+    @course = Course.find(params[:id])
+  end
 
 end
