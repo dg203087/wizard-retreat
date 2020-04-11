cbb1f4bb0ebcb08d651d8530ae4f3e1e2721a81a
github username <github email address>
2020-04-11 18:06:55 -0400

    add validations to wizard model

--- app/models/wizard.rb
+++ app/models/wizard.rb
@@ -2,4 +2,7 @@ class Wizard < ApplicationRecord
     has_secure_password
     has_many :registrations
     has_many :courses, through: :registrations
+    validates :email, uniqueness: true, presence: true
+    validates :password, uniqueness: true, length: {in: 6..20}, presence: true
+    # accepts_nested_attributes_for :courses
 end
