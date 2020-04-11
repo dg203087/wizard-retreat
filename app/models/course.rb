e2ec85e11f15abb770e5a9a11f5e7e39151d61c1
github username <github email address>
2020-04-11 18:06:29 -0400

    add validations to course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,8 @@
 class Course < ApplicationRecord
     has_many :registrations
     has_many :wizards, through: :registrations
+    validates :name, presence: true, length: {in: 3...75}, uniqueness: true
+    validates :location, presence: true
+    validates :desciption, presence: true, length: {maximum: 250}
+    validates :maximum_participants, presence: true
 end
