20009630fd5e2cbb704b74cbd4cae672c9c5fd2e
github username <github email address>
2020-04-14 00:10:41 -0400

    add method to count enrolled

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,19 @@
 class Course < ApplicationRecord
     has_many :registrations
     has_many :wizards, through: :registrations
+    accepts_nested_attributes_for :registrations
+    
     validates :name, presence: true, length: {in: 3...75}, uniqueness: true
     validates :location, presence: true
-    validates :desciption, presence: true, length: {maximum: 250}
+    validates :description, presence: true, length: {maximum: 250}
     validates :maximum_participants, presence: true
+
+    def currently_enrolled
+        self.wizards.size
+    end
+
+    # def self.space?
+    #     currently_enrolled + 1 <= maximum_participants
+    # end
+
 end
