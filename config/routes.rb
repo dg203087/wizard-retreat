7ea92c191218a61de4f45c95820c907ff27ba32c
github username <github email address>
2020-04-14 00:14:47 -0400

    add edit and update routes to wizards

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   root 'site#home'
-  resources :wizards, only: [:new, :create, :show]
+  resources :wizards, only: [:new, :create, :show, :edit, :update]
   resources :courses
+  resources :registrations, only: [:new, :create]
 
 
   # get '/signup', to: 'wizards#new', as: "signup"
   # post '/signup', to: 'wizards#create'
   get '/signin', to: 'sessions#new', as: "signin"
   post '/signin', to: 'sessions#create'
+  get '/auth/facebook/callback', to: 'sessions#facebook'
   get '/signout', to: 'sessions#destroy'
 
 end
