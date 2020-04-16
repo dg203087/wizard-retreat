23ac970ba8d46ff369292c98d4b4072de6e665f4
github username <github email address>
2020-04-16 00:21:38 -0400

    add edit and destroy routes for registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   root 'site#home'
-  resources :wizards, only: [:new, :create, :show, :edit, :update]
+  resources :wizards
   resources :courses
-  resources :registrations, only: [:new, :create]
+  resources :registrations, only: [:new, :create, :destroy] #do I need this route?
 
+  
+  resources :courses, only: [:index] do 
+    resources :registrations, only: [:index, :new, :edit, :update]
+  end 
 
-  # get '/signup', to: 'wizards#new', as: "signup"
-  # post '/signup', to: 'wizards#create'
   get '/signin', to: 'sessions#new', as: "signin"
   post '/signin', to: 'sessions#create'
   get '/auth/facebook/callback', to: 'sessions#facebook'
