619a644aabecf60a1e47c9ec9b0df3438a0f7515
github username <github email address>
2020-04-16 00:27:43 -0400

    create model class method to filter wizards by house

--- app/models/wizard.rb
+++ app/models/wizard.rb
@@ -2,7 +2,29 @@ class Wizard < ApplicationRecord
     has_secure_password
     has_many :registrations
     has_many :courses, through: :registrations
-    validates :email, uniqueness: true, presence: true
+
+    validates :email, uniqueness: true, presence: true, format: {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}, uniqueness: {message:"already belongs to an existing account"}
     validates :password, uniqueness: true, length: {in: 6..20}, presence: true
-    # accepts_nested_attributes_for :courses
+    validates_presence_of :first_name, :last_name, format: { with: /\A[a-zA-Z]+\Z/ }, message:"Name cannot be blank"
+   
+    def full_name
+        full_name = [first_name, last_name].join(' ')
+    end
+
+    def self.griffindor
+        where(wizards: {house: "Gryffindor"})
+    end
+
+    def self.slytherin
+        where(wizards: {house: "Slytherin"})
+    end
+
+    def self.ravenclaw
+        where(wizards: {house: "Ravenclaw"})
+    end
+
+    def self.hufflepuff
+        where(wizards: {house: "hufflepuff"})
+    end
+
 end
