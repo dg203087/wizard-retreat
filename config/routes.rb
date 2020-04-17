c46b38a3d15d622af025ecb63ae7cc958731e61d
github username <github email address>
2020-04-17 00:26:13 -0400

    add nested route for registrations/wizards

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
   root 'site#home'
-  resources :wizards
-  resources :courses
-  resources :registrations, only: [:new, :create, :destroy] #do I need this route?
-
-  
-  resources :courses, only: [:index] do 
-    resources :registrations, only: [:index, :new, :edit, :update]
-  end 
 
   get '/signin', to: 'sessions#new', as: "signin"
   post '/signin', to: 'sessions#create'
   get '/auth/facebook/callback', to: 'sessions#facebook'
   get '/signout', to: 'sessions#destroy'
+  
+  resources :wizards
+  resources :courses
+  resources :registrations, only: [:new, :create, :destroy] #do I need this route?
+
+  
+  resources :courses do 
+    resources :registrations
+  end 
+
+  resources :wizards, only: [:index] do 
+    resources :registrations, only: [:index]
+  end 
 
 end
